2f198df9e14baf31ea6873325ea14e7170d991d3
würthless elektroniks <wurthlesselektroniks@proton.me>
2025-11-26 15:56:20 -0500

    that's looking an awful lot like a milliseconds timestamp

--- source/lv2/main.c
+++ source/lv2/main.c
@@ -210,13 +210,15 @@ int main(){
 
 		if (rtc_response[0] == 4) {
 			printf("got RTC response from smc...\n");
-			printf("rtc = %02x %02x %02x %02x %02x\n",
-				rtc_response[1],
-				rtc_response[2],
-				rtc_response[3],
-				rtc_response[4],
-				rtc_response[5]
-			);
+
+			uint64_t time_in_ms =
+				rtc_response[1] |
+				(rtc_response[2] << 8) |
+				(rtc_response[3] << 16) |
+				(rtc_response[4] << 24) |
+				(rtc_response[5] << 32);
+				
+			printf("time in ms? = %ld\n", time_in_ms);
 
 			delay(1);
 		}
